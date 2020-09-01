52ebb22ee9c50b5442f93b2c5e3942b88ee30621
Senthilkannan <51955692+techkanna@users.noreply.github.com>
2020-09-01 08:26:54 +0530

    fixed home page issue

---
+++ docs/precache-manifest.a58596d22cfb55bc5123a810d225e237.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab1e660f5ec2cdb6dcb244e21f555935",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fb98a6d7ac1cb63328ba",
+    "url": "./static/css/main.b557eac6.chunk.css"
+  },
+  {
+    "revision": "b67b5bb9b3d04ca8b8a1",
+    "url": "./static/js/2.7c2454e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.7c2454e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb98a6d7ac1cb63328ba",
+    "url": "./static/js/main.fcc1e751.chunk.js"
+  },
+  {
+    "revision": "e6b9a28976d9dca2d4f7",
+    "url": "./static/js/runtime-main.5a0cd1c8.js"
+  }
+]);
